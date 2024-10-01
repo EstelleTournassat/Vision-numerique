63042cd25b81b222687b6b06d7a36a981ba400c8
Basset75233 <94084737+Basset75233@users.noreply.github.com>
2024-10-01 11:47:13 -0400

    Importation Basique Image et Fonction laminogram

--- recon.py
+++ recon.py
@@ -1,9 +1,16 @@
+import cv2
+import glob
+import numpy as np
+from scipy.ndimage import rotate
+
+# Fonction laminogram sans les .geo
+
 def laminogram(sinogram):
     # Calculer les paramètres à partir du sinograme
     nbprj, nbpix = sinogram.shape # il faut peut être inverser nbprj et nbpix, j'ai pas testé
     
     # Initialiser une image vide
-    image = np.ones((geo.nbpix, geo.nbpix))
+    image = np.ones((nbpix, nbpix))
 
     # Comme la deuxième moitié des projections est symétrique,
     # on peut traiter de seulement la première moitié
@@ -14,7 +21,7 @@ def laminogram(sinogram):
     for i in range(nbprj//2+1):
         # Répéter la projection sur plusieurs colonnes
         # pour l'étaler  sur toute l'image à reconstruire
-        sinogram_image = np.tile(sinogram[i], (geo.nbpix, 1))
+        sinogram_image = np.tile(sinogram[i], (nbpix, 1))
 
         # Tourne la projection étalée selon l'angle à laquelle elle a été prise
         rotated_list = rotate(sinogram_image, 360*i/(nbprj-1), reshape=False, order=0)
@@ -22,4 +29,30 @@ def laminogram(sinogram):
         # Ajouter les listes tournées
         image *= rotated_list
     
-    return sinogram
+    return image
+
+
+images = np.array([cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in glob.glob("Images/*.png")])/255
+# sans boucle, test rapide
+image_final = laminogram(images[:,220,:])
+print(image_final)
+#cv2.imshow("Title", image_final)
+
+#print(image_final.shape)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+for i in range(0,480,10):
+    
+    image_final = laminogram(images[:,i,:])
+    #print(image_final)
+    cv2.imshow("Title", image_final)
+    #cv2.waitKey(0)
+
+    #cv2.destroyAllWindows()
+    #print(images[:,i,:])
+    
+
+
+
